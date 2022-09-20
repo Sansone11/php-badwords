d0caf5bd3a721af7ae3a5796fb544cecce5c5e19
Sansone11 <sansonemirco@gmail.com>
2022-09-20 15:49:30 +0200

    text-print

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$Dante_speak = 'Per me si va ne la città dolente,
+                per me si va ne l’etterno dolore,
+                per me si va tra la perduta gente.
+                Giustizia mosse il mio alto fattore:
+                fecemi la divina podestate,
+                la somma sapienza e ’l primo amore.
+                Dinanzi a me non fuor cose create
+                se non etterne, e io etterno duro.
+                Lasciate ogne speranza, voi ch’intrate”.
+                Queste parole di colore oscuro
+                vid’io scritte al sommo d’una porta;
+                per ch’io: «Maestro, il senso lor m’è duro';
+$string_length = strlen( $Dante_speak )
+?>
+<h1><?php echo $Dante_speak ?></h1>
+<p>La stinga è lunga <?php echo $string_length ?></p>
